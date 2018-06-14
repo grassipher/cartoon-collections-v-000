837bd151a6cf4f4f838e1cab50af2c0679c1b9d6
Gabriel Valle <gvalle91@gmail.com>
2018-06-14 19:57:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(array)
   end
 end
 
-def long_planeteer_calls(array))
+def long_planeteer_calls(array)
   word_tester = false
   array.each do |name|
     word_tester = true if name.length > 4
